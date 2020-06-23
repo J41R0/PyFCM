2d708f352a9e853d2d3f25bf24abf52739ae4821
Jairo Lefebre <yodax@aleph.engineering>
2020-06-23 13:58:02 -0400

    Update minor library version to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='Py_FCM',
-    version='0.2.1',
+    version='0.2.2',
     scripts=[],
     author="Jairo Lefebre",
     author_email="jairo.lefebre@gmail.com",
