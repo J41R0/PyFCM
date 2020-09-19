ba678419f6aa4c89f770e8dbced93cb99e8688eb
Jairo Lefebre <yodax@aleph.engineering>
2020-09-19 12:41:57 -0400

    Add create_concept function to tests __init__.py   To generate concepts for testing purposes

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+from Py_FCM.__const import *
+from Py_FCM.functions import Excitation
+
+
+def create_concept(concept_name: str, node_type=TYPE_SIMPLE, is_active=True, use_memory=None,
+                   exitation_function='KOSKO', activation_dict=None, activ_function=None, **kwargs):
+    test_concept = {NODE_ACTIVE: is_active, NODE_ARCS: [], NODE_AUX: [], NODE_VALUE: 0.0}
+    test_concept[NODE_TYPE] = node_type
+    test_concept[NODE_EXEC_FUNC] = Excitation.get_by_name(exitation_function)
