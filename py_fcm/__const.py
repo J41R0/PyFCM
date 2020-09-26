afa499e67eb274e81893eb831a038641f110e65d
Jairo Lefebre <yodax@aleph.engineering>
2020-09-26 19:25:00 -0400

    Refactor __const.py  Rename relations const names changing ARC prefix to RELATION  Add node function names related const

--- py_fcm/__const.py
+++ py_fcm/__const.py
@@ -16,9 +16,9 @@ FUNC_PULSE = 6
 FUNC_MEAN_GAP = 7
 
 # relations const
-ARC_DESTINY = 0
-ARC_WEIGHT = 1
-ARC_ORIGIN = 2
+RELATION_DESTINY = 0
+RELATION_WEIGHT = 1
+RELATION_ORIGIN = 2
 
 # topology const
 NODE_ACTIVE = "current node is active or not"  # non active nodes do not activate neighbors
@@ -28,7 +28,9 @@ NODE_VALUE = "current node value"
 NODE_USE_MEM = "use memory in current node execution"
 NODE_AUX = "auxiliary influence value list for new value calculation"
 NODE_EXEC_FUNC = "custom execution function"
+NODE_EXEC_FUNC_NAME = "custom execution function name"
 NODE_ACTV_FUNC = "custom activation function"
+NODE_ACTV_FUNC_NAME = "custom activation function name"
 NODE_ACTV_FUNC_ARGS = "custom activation function arguments"
 NODE_TRAIN_ACTIVATION = "activation relation for continous values"
 NODE_TRAIN_MIN = "minimum continous value"
@@ -40,4 +42,4 @@ TYPE_DECISION = "node type for classification problems"
 TYPE_FUZZY = "node type for fuzzy treatment of continous values"
 TYPE_REGRESOR = "node type for regression problems"
 TYPE_MUTI = "node type for multivalues fields"
-TYPE_MUTI_DESC = "node type for multivalues fields in desition nodes"
+TYPE_MUTI_DESC = "node type for multivalues fields in decision nodes"
