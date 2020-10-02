e7f26410b0f5f709ec4fd17c7f2e9234a264ea6f
Jairo Lefebre <yodax@aleph.engineering>
2020-10-02 18:02:29 -0400

    Add function is_valid_type   To know if a node type is valid or not

--- py_fcm/__const.py
+++ py_fcm/__const.py
@@ -43,3 +43,14 @@ TYPE_FUZZY = "node type for fuzzy treatment of continous values"
 TYPE_REGRESOR = "node type for regression problems"
 TYPE_MUTI = "node type for multivalues fields"
 TYPE_MUTI_DESC = "node type for multivalues fields in decision nodes"
+
+
+def is_valid_type(node_type) -> bool:
+    type_set = set()
+    type_set.add(TYPE_SIMPLE)
+    type_set.add(TYPE_DECISION)
+    type_set.add(TYPE_FUZZY)
+    type_set.add(TYPE_REGRESOR)
+    type_set.add(TYPE_MUTI)
+    type_set.add(TYPE_MUTI_DESC)
+    return node_type in type_set
