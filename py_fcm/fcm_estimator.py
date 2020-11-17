58636271b444fd3f361e7d1df6cc500d00baf133
Jairo Lefebre <yodax@aleph.engineering>
2020-11-17 17:58:26 -0500

    Add predict method implementation

--- py_fcm/fcm_estimator.py
+++ py_fcm/fcm_estimator.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from pandas import DataFrame
 
 from py_fcm.fcm import FuzzyCognitiveMap
@@ -11,6 +13,7 @@ class FcmEstimator:
                  concept_str_separator="___", fit_inclination=False, causal_eval_function=Relation.conf,
                  causal_threshold=0, causality_function=Relation.conf, causal_sign_function=None,
                  causal_sign_threshold=0, fcm_activation_function="sigmoid_hip", **kwargs):
+        self.__is_fcm_generated = False
         # init FCM
         self.__fcm = FuzzyCognitiveMap(max_it=fcm_max_it,
                                        extra_steps=fcm_extra_steps,
@@ -20,6 +23,7 @@ class FcmEstimator:
                                        activation_function=fcm_activation_function,
                                        **kwargs)
         self.__fcm.set_default_concept_properties(excitation_function=fcm_excitation_function)
+        self.__fcm.debug = False
         # init generator
         self.__generator = AssociationBasedFCM(str_separator=concept_str_separator,
                                                fit_inclination=fit_inclination,
@@ -36,9 +40,21 @@ class FcmEstimator:
             target_feat.append(col)
         x.join(y, rsuffix='_class')
         self.__generator.build_fcm(x, target_features=target_feat, fcm=self.__fcm)
+        self.__is_fcm_generated = True
 
     def predict(self, x: DataFrame):
-        pass
+        result = defaultdict(list)
+        if self.__is_fcm_generated:
+            for index, row in x.iterrows():
+                for feat_name in x.columns:
+                    self.__generator.init_concept_by_feature_data(feat_name, row[feat_name])
+
+            prediction = self.__generator.get_inference_result()
+            for feat_name in prediction:
+                result[feat_name].append(prediction[feat_name])
+        else:
+            raise Exception("There is no FCM generated, the fit method mus be called first")
+        return DataFrame(result)
 
     def score(self, x: DataFrame, y: DataFrame):
         pass
