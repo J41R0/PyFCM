4e77a0f4175e7160591880c49460e7d3e279cbdf
Jairo Lefebre <yodax@aleph.engineering>
2020-11-17 19:35:51 -0500

    Add FcmEstimator to library __init__.py

--- py_fcm/__init__.py
+++ py_fcm/__init__.py
@@ -1,2 +1,3 @@
+from py_fcm.fcm_estimator import FcmEstimator
 from py_fcm.loader import from_json, join_maps, FuzzyCognitiveMap
 from py_fcm.utils.__const import TYPE_SIMPLE, TYPE_DECISION, TYPE_FUZZY
