8b283f48f972679972ab919c706861fa4a5e699f
Jairo Lefebre <yodax@aleph.engineering>
2020-11-17 20:24:37 -0500

    Refactor class FcmEstimator   Add __to_data_frame function to transform input data into a DataFrame object if required   Add __validate_dual_input to transform non feature labeled data into a DataFrame object   Refactor fir, predict, and score method to use the mentioned functions and fix some issues found in the implementation

--- py_fcm/fcm_estimator.py
+++ py_fcm/fcm_estimator.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 
+import numpy as np
 from pandas import DataFrame
 
-from py_fcm.fcm import FuzzyCognitiveMap
 from py_fcm.utils.functions import Relation
-from py_fcm.learning.association import AssociationBasedFCM
+from py_fcm.learning.association import AssociationBasedFCM, FuzzyCognitiveMap
 
 
 class FcmEstimator:
@@ -34,30 +34,56 @@ class FcmEstimator:
                                                sign_function=causal_sign_function,
                                                sign_threshold=causal_sign_threshold)
 
-    def fit(self, x: DataFrame, y: DataFrame):
+    def __to_data_frame(self, x, start=0):
+        if type(x) == list:
+            x = np.array(x)
+        if type(x) == np.ndarray:
+            col_names = []
+            if len(x.shape) == 1:
+                col_names.append(str(start))
+            else:
+                col_names = [str(i + start) for i in range(x.shape[1])]
+            return DataFrame(x, columns=col_names)
+
+    def __validate_dual_input(self, x, y):
+        if type(x) != type(y):
+            raise Exception("Different dataset types not allowed")
+        if type(x) == list:
+            x = np.array(x)
+        max_col = x.shape[1]
+        x = self.__to_data_frame(x)
+        y = self.__to_data_frame(y, start=max_col)
+        return x, y
+
+    def fit(self, x, y):
+        # expect a dataframe or a matrix shaped in a list of rows
+        x, y = self.__validate_dual_input(x, y)
         target_feat = []
         for col in y.columns:
             target_feat.append(col)
-        x.join(y, rsuffix='_class')
-        self.__generator.build_fcm(x, target_features=target_feat, fcm=self.__fcm)
+        input_data = x.join(y, rsuffix='_class')
+        self.__generator.build_fcm(input_data, target_features=target_feat, fcm=self.__fcm)
         self.__is_fcm_generated = True
 
     def predict(self, x: DataFrame):
+        if type(x) != DataFrame:
+            x = self.__to_data_frame(x)
         result = defaultdict(list)
         if self.__is_fcm_generated:
             for index, row in x.iterrows():
                 for feat_name in x.columns:
                     self.__generator.init_concept_by_feature_data(feat_name, row[feat_name])
 
-            prediction = self.__generator.get_inference_result()
-            for feat_name in prediction:
-                result[feat_name].append(prediction[feat_name])
+                prediction = self.__generator.get_inference_result()
+                for feat_name in prediction:
+                    result[feat_name].append(prediction[feat_name])
         else:
             raise Exception("There is no FCM generated, the fit method mus be called first")
         return DataFrame(result)
 
-    def score(self, x: DataFrame, y: DataFrame):
+    def score(self, x, y):
         if self.__is_fcm_generated:
+            x, y = self.__validate_dual_input(x, y)
             predicted_result = self.predict(x)
             right_predicted = 0
             total = 0
